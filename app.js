1a58821d5b8d61b292817c14414da028383e8d1d
zbshadow <zbshadowx@gmail.com>
2020-10-04 05:45:42 -0400

    First Draft of Home Screen, several changes.
    On branch Home-Screen
    Changes to be committed:
    	new file:   app.js
            currently controls dropdown menu on home screen. Later will control several backend items.
    	new file:   imgs/WOWOTL_Logo_v10_edit.png
    	new file:   imgs/kisspng-world-of-warcraft-legion-expansion-pack-desktop-w-legion-wow-5b50188a316a43.5196361415319758182024.png
    	modified:   index.html
            Changed from login screen to home page. First Draft of page.
    	new file:   login.html
            old index.html. Renamed to login.html, Links site wide updated.
    	modified:   style.css
            adjusted styles for new index.html page.

---
+++ app.js
@@ -0,0 +1,17 @@
+/*Handles Drop Down button onClick event*/
+function dropDownFunction() {
+    document.getElementById("colDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("cd-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
